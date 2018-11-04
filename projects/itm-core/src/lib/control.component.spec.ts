c947dde179126286e7c31c568a7981ce119e1805
guduf <gudufour@gmail.com>
2018-11-04 17:30:28 +0100

    fix control test

--- projects/itm-core/src/lib/control.component.spec.ts
+++ projects/itm-core/src/lib/control.component.spec.ts
@@ -3,7 +3,7 @@ import { MatFormFieldModule } from '@angular/material';
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { of } from 'rxjs';
 
-import ControlRef from './control_ref';
+import ControlRef, { ITM_CONTROL_REF } from './control_ref';
 import { ItmControlComponent } from './control.component';
 import { ItmFieldLabel } from './field';
 
@@ -23,7 +23,7 @@ describe('ItmControlComponent', () => {
       ],
       providers: [
         {provide: ItmFieldLabel, useValue: fieldLabel},
-        {provide: ControlRef, useValue: ngControl}
+        {provide: ITM_CONTROL_REF, useValue: ngControl}
       ]
     });
     TestBed.compileComponents();
